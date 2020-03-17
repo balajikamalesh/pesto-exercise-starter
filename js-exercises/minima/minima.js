32e509a8c1848a6f1721c400169cadba4c2da446
abalajikamalesh <abalajikamalesh@gmail.com>
2020-03-17 18:21:22 +0530

    solution minima

---
+++ js-exercises/minima/minima.js
@@ -0,0 +1,19 @@
+function minima(k, arr) {
+  let len = arr.length;
+    for (let i = 0; i < k; i++) {
+        let min = i;
+        for (let j = i + 1; j < len; j++) {
+            if (arr[min] > arr[j]) {
+                min = j;
+            }
+        }
+        if (min !== i) {
+            let tmp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = tmp;
+        }
+    }
+    return arr.slice(0, k);
+}
+
+export { minima };
