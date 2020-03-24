cb01ce382cebd3112f5fd1e19227464148523e2f
abalajikamalesh <abalajikamalesh@gmail.com>
2020-03-24 17:12:14 +0530

    solution sequential-promise

---
+++ js-exercises/sequential-promise/sequentialPromise.js
@@ -0,0 +1,9 @@
+const sequentialPromise = promises => {
+  var promise = promises[0]();
+  for (var i = 1; i < promises.length; i++)
+      promise = promise.then(promises[i]);
+
+  return new Promise((resolve) => resolve(promise));
+};
+
+export { sequentialPromise };
