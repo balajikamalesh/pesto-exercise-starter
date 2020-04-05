2b79fad980750b70d33c158f8444a993596c715f
abalajikamalesh <abalajikamalesh@gmail.com>
2020-04-05 21:31:45 +0530

    Updates

---
+++ precache-manifest.31fc1b5553cd6522797e21c8fff42574.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50cb6c2bdd205cb06f289e00dc23a734",
+    "url": "/pesto-exercise-starter/index.html"
+  },
+  {
+    "revision": "d34c24f84715843c11d8",
+    "url": "/pesto-exercise-starter/static/css/main.29fdddb7.chunk.css"
+  },
+  {
+    "revision": "a25204b7c8a044107907",
+    "url": "/pesto-exercise-starter/static/js/2.756b725d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pesto-exercise-starter/static/js/2.756b725d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d34c24f84715843c11d8",
+    "url": "/pesto-exercise-starter/static/js/main.6876d9c4.chunk.js"
+  },
+  {
+    "revision": "3704eefe8fe85cdfd140",
+    "url": "/pesto-exercise-starter/static/js/runtime-main.38210067.js"
+  },
+  {
+    "revision": "a36764134107d0fe6c80bc7fa696fb16",
+    "url": "/pesto-exercise-starter/static/media/smb_coin.a3676413.wav"
+  },
+  {
+    "revision": "9f02f1b1b02d5ff0b9b315c4c9ebb0da",
+    "url": "/pesto-exercise-starter/static/media/smb_mariodie.9f02f1b1.wav"
+  }
+]);
