cefa3dbf17dd17d6ddec40dd7d78c09325f02e48
abalajikamalesh <abalajikamalesh@gmail.com>
2020-04-06 19:32:25 +0530

    Scroll issue fixed

--- react-exercises/snake-game-react/src/App.js
+++ react-exercises/snake-game-react/src/App.js
@@ -61,6 +61,9 @@ function App() {
   },[direction, isGameOver, speed, moveSnake, onKeyDown, snake])
 
   function onKeyDown(event){
+    if([32, 37, 38, 39, 40].indexOf(event.keyCode) > -1) {
+      event.preventDefault();
+    } 
     if(event.keyCode === 38 && direction !== 'D')
       setDirection('U');  
     if(event.keyCode === 39 && direction !== 'L')
